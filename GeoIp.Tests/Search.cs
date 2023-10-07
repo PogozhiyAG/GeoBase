a7974efee43ca2928d43d1eca90c79ca4751c6f5
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-10-07 23:46:14 +0100

    add test cases

--- GeoIp.Tests/Search.cs
+++ GeoIp.Tests/Search.cs
@@ -6,24 +6,36 @@ public class Search
 {
     private readonly GeoIpDatabase dataBase;
 
+
+
     public Search()
     {
         dataBase = new GeoIpDatabase(File.ReadAllBytes("geobase.dat"));
     }
 
-    [Fact]
-    public void Search_by_city_name_exact()
+    [Theory]
+    [InlineData("cit_Epimyj", StringSearchMode.Exact, true)]
+    [InlineData("cit_Ep", StringSearchMode.Starts, true)]
+    [InlineData("AAA", StringSearchMode.Exact, false)]
+    [InlineData("AAA", StringSearchMode.Starts, false)]
+    public void Search_by_city_name(string cityName, StringSearchMode searchMode, bool shouldBeFound)
     {
-        string cityName = "cit_Epimyj";
-        var searchResults = new List<Location.Managed>();
-
+        int count = 0;
         dataBase.FindLocationsByCity(cityName, StringSearchMode.Exact, location =>
         {
-            searchResults.Add(location);
+            Assert.True(
+                searchMode switch
+                {
+                    StringSearchMode.Exact => location.City == cityName,
+                    StringSearchMode.Starts => location.City!.StartsWith(cityName),
+                    _ => false
+                }
+            );
+            count++;
             return true;
         });
 
-        Assert.True(searchResults.Any());
-        Assert.True(searchResults.TrueForAll(l => l.City == cityName));
+        Assert.True(!shouldBeFound || count > 0);
+
     }
 }
